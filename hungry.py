d98c9668fba9b8639e84250acaf15db1f4fc4805
Irfan Sadat <95441141+irfansadat99@users.noreply.github.com>
2024-08-28 01:38:32 +0500

    added y as  a valid answer irfan

--- hungry.py
+++ hungry.py
@@ -1,7 +1,7 @@
 hungry=input("are you hungry?")
-if hungry=="yes" and hungry=="Y" and hungry=="y":
+if hungry=="yes" and hungry=='y':
     print("eat samosa")
     print("eat pizza")
     print("eat burger")
 else:
-    print("do homework")    
+    print("do homework irfan")    
